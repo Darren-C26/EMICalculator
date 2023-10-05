957cd18a41c6793c80c3b97771f58f95c5d4a5c8
Darren-C26 <decons2602@gmail.com>
2023-10-04 23:55:44 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/emicalculator/MainActivity.java
+++ app/src/main/java/com/example/emicalculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.*;
 import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
+    // UI Elements are declared for input fields and calculate button
     EditText et_principal;
     EditText et_tenure;
     EditText et_interest;
@@ -17,20 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialized UI elements
         et_principal = findViewById(R.id.et_principal);
         et_tenure = findViewById(R.id.et_tenure);
         et_interest = findViewById(R.id.et_interest);
         b_calculate = findViewById(R.id.b_calculate);
 
+        // Set a click listener for the "Calculate EMI" button
         b_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calculateEMI();
-            }
+            } // calcualteEMI() method is called
         });
 
     }
 
+    // Calculate EMI method
     private void calculateEMI() {
         // Get user input
         double principalAmount = Double.parseDouble(et_principal.getText().toString());
@@ -44,11 +48,14 @@ public class MainActivity extends AppCompatActivity {
         double emi = (principalAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, tenureMonths))
                 / (Math.pow(1 + monthlyInterestRate, tenureMonths) - 1);
 
+        // Calculate total paid and interest paid
         double total_paid = emi * tenureMonths;
         double interest_paid = total_paid - principalAmount;
 
+        // Create Intent to navigate to PaymentResult activity
         Intent intent = new Intent(MainActivity.this, PaymentResult.class);
 
+        // Add key-value pairs to Intent to pass onto PaymentResult
         intent.putExtra("mortgage", String.format("$%.2f",principalAmount));
         intent.putExtra("interest", et_interest.getText().toString() + "%");
         intent.putExtra("tenure", et_tenure.getText().toString());
@@ -56,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("total_paid", String.format("$%.2f",total_paid));
         intent.putExtra("interest_paid", String.format("$%.2f",interest_paid));
 
+        // PaymentResult activity is started
         startActivity(intent);
 
     }
